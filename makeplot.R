337c0d2b74077b77e5180d2ef8d008329145e0f3
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2016-07-31 16:04:34 -0400

    Better labels

--- makeplot.R
+++ makeplot.R
@@ -1,4 +1,4 @@
 library(ggplot2)
 X = read.delim('~/Desktop/poker-stats/hand_ranks.txt', header=FALSE)
-qplot(x=V1, fill=V2, data=X)
+qplot(x=V1, fill=V2, data=X) + labs(x = "Hand rank", fill = "Hand type")
 table(X$V2) / length(X$V2) * 100
