5fab20719cc12aab980c42d13f9d0c5cfdf65655
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2016-07-31 15:18:12 -0400

    Two functions that helped us in the past.

---
+++ convenience.py
@@ -0,0 +1,14 @@
+def pr(x):
+    Card.print_pretty_cards(x)
+
+def who_wins(b, p1, p2):
+    [pr(h) for h in [b, p1, p2]]
+    s = [evaluator.evaluate(b, p) for p in [p1, p2]]
+    r = [evaluator.class_to_string(evaluator.get_rank_class(x)) for x in s]
+    if s[1] > s[0]:
+        t = "P1 wins"
+    elif s[1] < s[0]:
+        t = "P2 wins"
+    else:
+        t = "push"
+    print ', '.join(map(str, s) + map(str, r) + [t])
