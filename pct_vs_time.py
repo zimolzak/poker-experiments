c43e6a69fa1391b5fd00a43628111f8d52ec8792
Andrew J. Zimolzak <andyzimolzak@gmail.com>
2016-08-11 08:47:32 -0400

    Make find_pcts() function that can repetitively run.

--- pct_vs_time.py
+++ pct_vs_time.py
@@ -1,21 +1,25 @@
 from deuces.deuces import Card, Deck
-from convenience import who_wins
+from convenience import who_wins, pr
+from copy import deepcopy
 
 p1 = [Card.new('As'), Card.new('Ac')]
 p2 = [Card.new('Ad'), Card.new('Kd')]
-win_record = []
 
-for i in range(100000):
-    deck = Deck()
-    b = []
-    while len(b) < 5:
-        c = deck.draw()
-        if c in p1 or c in p2:
-            continue
-        b.append(c)
-    win_record.append(who_wins(b, p1, p2, printout = False))
+def find_pcts(p1, p2, start_b = [], iter = 10000):
+    win_record = []
+    for i in range(iter):
+        deck = Deck()
+        b = deepcopy(start_b)
+        while len(b) < 5:
+            c = deck.draw()
+            if c in p1 + p2 + b:
+                continue
+            b.append(c)
+        win_record.append(who_wins(b, p1, p2, printout = False))
+    return [win_record.count(1) / float(len(win_record)), 
+            win_record.count(2) / float(len(win_record))
+    ]
 
 Card.print_pretty_cards(p1)
-print win_record.count(1) / float(len(win_record))
 Card.print_pretty_cards(p2)
-print win_record.count(2) / float(len(win_record))
+print find_pcts(p1, p2)
