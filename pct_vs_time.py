18120a3e7ce2159bbe9ffd815eb3b40a68e7fcf8
Andrew J. Zimolzak <andyzimolzak@gmail.com>
2016-08-11 08:33:12 -0400

    Just print win percentages, many more iterations.

--- pct_vs_time.py
+++ pct_vs_time.py
@@ -1,12 +1,21 @@
-from deuces.deuces import Card, Evaluator, Deck
+from deuces.deuces import Card, Deck
 from convenience import who_wins
-
-evaluator = Evaluator()
 
 p1 = [Card.new('As'), Card.new('Ac')]
 p2 = [Card.new('Ad'), Card.new('Kd')]
+win_record = []
 
-for i in range(10):
+for i in range(100000):
     deck = Deck()
-    b = deck.draw(5)
-    print who_wins(b, p1, p2, printout = False)
+    b = []
+    while len(b) < 5:
+        c = deck.draw()
+        if c in p1 or c in p2:
+            continue
+        b.append(c)
+    win_record.append(who_wins(b, p1, p2, printout = False))
+
+Card.print_pretty_cards(p1)
+print win_record.count(1) / float(len(win_record))
+Card.print_pretty_cards(p2)
+print win_record.count(2) / float(len(win_record))
