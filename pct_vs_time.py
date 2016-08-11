faba07a445ec85b463e5e3d96ec440cd83544b72
Andrew J. Zimolzak <andyzimolzak@gmail.com>
2016-08-11 09:05:27 -0400

    Apply function in one new place

--- pct_vs_time.py
+++ pct_vs_time.py
@@ -5,25 +5,6 @@ from copy import deepcopy
 p1 = [Card.new('As'), Card.new('Ac')]
 p2 = [Card.new('Ad'), Card.new('Kd')]
 
-def find_pcts(p1, p2, start_b = [], iter = 10000):
-    win_record = []
-    for i in range(iter):
-        deck = Deck()
-        b = deepcopy(start_b)
-        while len(b) < 5:
-            c = deck.draw()
-            if c in p1 + p2 + b:
-                continue
-            b.append(c)
-        win_record.append(who_wins(b, p1, p2, printout = False))
-    return [win_record.count(1) / float(len(win_record)), 
-            win_record.count(2) / float(len(win_record))
-    ]
-
-Card.print_pretty_cards(p1)
-Card.print_pretty_cards(p2)
-print find_pcts(p1, p2)
-
 def draw_sure(deck, n, exclusions):
     drawn = []
     while len(drawn) < n:
@@ -32,7 +13,22 @@ def draw_sure(deck, n, exclusions):
             continue
         drawn.append(c)
     return drawn
-    
+
+def find_pcts(p1, p2, start_b = [], iter = 10000):
+    win_record = []
+    for i in range(iter):
+        deck = Deck()
+        need = 5 - len(start_b)
+        b2 = draw_sure(deck, need, p1+p2+start_b)
+        win_record.append(who_wins(start_b + b2, p1, p2, printout = False))
+    return [win_record.count(1) / float(len(win_record)), 
+            win_record.count(2) / float(len(win_record))
+    ]
+
+Card.print_pretty_cards(p1)
+Card.print_pretty_cards(p2)
+print find_pcts(p1, p2)
+
 main_deck = Deck()
 
 flop = draw_sure(main_deck, 3, p1 + p2)
