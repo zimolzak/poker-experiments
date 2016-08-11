95bcf7839ef3b0cf307358476153294958aacdcc
Andrew J. Zimolzak <andyzimolzak@gmail.com>
2016-08-11 09:00:05 -0400

    Draw for flop, turn, river and show percents each time.

--- pct_vs_time.py
+++ pct_vs_time.py
@@ -23,3 +23,26 @@ def find_pcts(p1, p2, start_b = [], iter = 10000):
 Card.print_pretty_cards(p1)
 Card.print_pretty_cards(p2)
 print find_pcts(p1, p2)
+
+def draw_sure(deck, n, exclusions):
+    drawn = []
+    while len(drawn) < n:
+        c = deck.draw()
+        if c in exclusions + drawn:
+            continue
+        drawn.append(c)
+    return drawn
+    
+main_deck = Deck()
+
+flop = draw_sure(main_deck, 3, p1 + p2)
+pr(flop)
+print find_pcts(p1, p2, start_b = flop)
+
+turn = draw_sure(main_deck, 1, p1+p2+flop)
+pr(turn)
+print find_pcts(p1, p2, start_b = flop+turn)
+
+river = draw_sure(main_deck, 1, p1+p2+flop+turn)
+pr(river)
+print find_pcts(p1, p2, start_b = flop+turn+river)
