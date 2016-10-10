1092d433141ae27bce1e53b34b7fe1482aae9a0c
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-10-10 18:42:32 -0400

    Successfully parse command line hands description.

--- one_time_eval.py
+++ one_time_eval.py
@@ -1,11 +1,17 @@
+# usage: python one_time_eval.py as8sqdtc
+
 from deuces.deuces import Card
-from convenience import find_pcts
+from convenience import find_pcts, pr
 import sys
 
-card_str = sys.argv[1]
-assert len(card_str) == 8
+s = sys.argv[1]
+assert len(s) == 8
+cards = [s[0:2], s[2:4], s[4:6], s[6:8]]
+for i, ci in enumerate(cards):
+    cards[i] = ci[0].upper() + ci[1].lower()
 
-
-p1 = [Card.new('As'), Card.new('Kc')]
-p2 = [Card.new('7h'), Card.new('7d')]
+p1 = [Card.new(cards[0]), Card.new(cards[1])]
+p2 = [Card.new(cards[2]), Card.new(cards[3])]
+pr(p1)
+pr(p2)
 print find_pcts(p1, p2, start_b = [], iter = 10000)
