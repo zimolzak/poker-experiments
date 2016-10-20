dd3b135ab5e229c5f717fc4d296389e187a49f9a
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-10-20 13:11:53 -0400

    Prepare the main script to accept multi-way pots.

--- one_time_eval.py
+++ one_time_eval.py
@@ -1,21 +1,29 @@
 # usage: python one_time_eval.py as8sqdtc
+# usage: python one_time_eval.py as8sqdtc 2skskd
 
-from deuces.deuces import Card
 from convenience import find_pcts, pr, str2cards
 import sys
 
-cards_str = sys.argv[1]
+## argv to strings
+hole_cards_str = sys.argv[1]
 board_str = ''
 if len(sys.argv) > 2:
     board_str = sys.argv[2]
 
-cards = str2cards(cards_str)
+## strings to lists of Card objects
+hole_cards = str2cards(hole_cards_str)
 board = str2cards(board_str)
 
-assert len(cards) == 4
-p1 = cards[0:2]
-p2 = cards[2:4]
+## card list to player list-of-lists
+assert len(hole_cards) % 2 == 0
+n_players = len(hole_cards) / 2
+assert n_players > 1
+p = []
+for i in range(n_players):
+    pi = hole_cards[i * 2 : i * 2 + 2]
+    pr(pi)
+    p.append(pi)
 
-pr(p1)
-pr(p2)
-print find_pcts(p1, p2, board, iter = 10000)
+print "Board",
+pr(board)
+print find_pcts(p[0], p[1], board, iter = 10000)
