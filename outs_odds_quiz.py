9b90a300aab649aca7d10801c0b0cb537185be75
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-11-05 16:19:46 -0400

    Non working code halfway between sort of numerical and closed form.

--- outs_odds_quiz.py
+++ outs_odds_quiz.py
@@ -13,15 +13,19 @@ pr(board)
 rc[0] = e.get_rank_class(e.evaluate(hole, board))
 print e.class_to_string(rc[0])
 
-# start looping here
-newdeck = Deck()
-riv = draw_sure(newdeck, 1, board + hole)
-pr(riv)
-board = board + riv
-rc[1] = e.get_rank_class(e.evaluate(hole, board))
-print e.class_to_string(rc[1])
+hits = 0
+maxiter = 52 - 5
 
-if rc[0] > rc[1]:
-    print "IMPROVED"
-else:
-    print "MISSED"
+for i in range(maxiter):
+    newdeck = Deck()
+    riv = draw_sure(newdeck, 1, board + hole)
+#    pr(riv)
+    board = board + riv
+    rc[1] = e.get_rank_class(e.evaluate(hole, board))
+#    print e.class_to_string(rc[1])
+    if rc[0] > rc[1]:
+        hits = hits + 1
+
+print hits / float(maxiter)
+
+### fixme adding all kinds of crap to board. decl new var called flop
