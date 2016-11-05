94dd4b2c52d7d1e4453df0bfeb98e2769a81c3ce
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-11-05 13:53:02 -0400

    Beginnings of working code to find outs / drawing odds.

---
+++ outs_odds_quiz.py
@@ -0,0 +1,27 @@
+from deuces.deuces import Evaluator, Deck
+from convenience import pr
+
+e = Evaluator()
+deck = Deck()
+hole = deck.draw(2)
+pr(hole)
+
+rc = [None] * 2
+
+board = deck.draw(3)
+pr(board)
+rc[0] = e.get_rank_class(e.evaluate(hole, board))
+print rc[0]
+print e.class_to_string(rc[0])
+
+riv = [deck.draw(1)]
+pr(riv)
+board = board + riv
+rc[1] = e.get_rank_class(e.evaluate(hole, board))
+print rc[1]
+print e.class_to_string(rc[1])
+
+if rc[0] > rc[1]:
+    print "IMPROVED"
+else:
+    print "MISSED"
