283aea39bafbae6d4acdbc399abd2cebfad77660
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-11-06 01:56:45 -0400

    If turn card pairs to flop, it is kind of not an out, but only usually.

--- outs_odds_quiz.py
+++ outs_odds_quiz.py
@@ -1,10 +1,22 @@
-from deuces.deuces import Evaluator, Deck
+from deuces.deuces import Evaluator, Deck, Card
 from convenience import pr, draw_sure
 
 e = Evaluator()
 deck = Deck()
 hole = deck.draw(2)
 pr(hole)
+
+def card_pairs_board(c, b):
+    card_rank = Card.get_rank_int(c)
+    board_ranks = map(Card.get_rank_int, b)
+    return card_rank in board_ranks
+
+def deck_without(cards):
+    returnme = []
+    for c in Deck.GetFullDeck():
+        if c not in cards:
+            returnme.append(c)
+    return returnme
 
 rc = [None] * 2
 
@@ -18,15 +30,28 @@ maxiter = 52 - 5
 outs = []
 
 for i in range(maxiter):
-    riv = deck.draw(1)
-    board = flop + [riv]
-    rc[1] = e.get_rank_class(e.evaluate(hole, board))
-    if rc[0] > rc[1]:
+    turn = deck.draw(1) # runs every card in deck
+    rc[1] = e.get_rank_class(e.evaluate(hole, flop + [turn]))
+    if rc[0] > rc[1] and not card_pairs_board(turn, flop):
         n_outs = n_outs + 1
-        outs.append(riv)
+        outs.append(turn)
 
 print n_outs, "outs"
 pr(outs)
 proportion = n_outs / float(maxiter)
 print round(proportion, 3)
 print round((1 - proportion) / proportion, 1), "to 1"
+
+## probably exclude any turn card that pairs to the board.
+## would cover: 2pr to board, trips to board, quads to board.
+## because only 4 card board, no chance of flush/str8 to board
+
+# bug = test case
+
+#   [ 6 h ] , [ J s ]  
+#   [ 4 c ] , [ J d ] , [ 3 s ]  
+# Pair
+# 3 outs
+#   [ 6 s ] , [ 6 d ] , [ 6 c ]  
+# 0.064
+# 14.7 to 1
