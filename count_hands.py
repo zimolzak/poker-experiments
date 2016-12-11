c2c08bb7920580daf884dbd89f26987e8af81a04
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-12-11 13:09:47 -0500

    More compact printing

--- count_hands.py
+++ count_hands.py
@@ -2,9 +2,11 @@ from convenience_hole import HR, deck_choose_2, numbers_of_hole_cards
 
 [Table, cells] = numbers_of_hole_cards()
 for s in cells:
-    print s, '\t', Table[s]
+    print s, Table[s], '\t',
+print
 print
 print sum(Table.values())
+print
 print
 
 HR_tot = sum([row['n'] for row in HR])
@@ -14,6 +16,7 @@ for s in cells:
         if row['h'] == s:
             n_hands_HR = float(row['n']) / HR_tot * deck_choose_2
             float_tot += n_hands_HR
-            print s, '\t', round(n_hands_HR, 2)
+            print s, round(n_hands_HR, 1), '\t',
+print
 print
 print float_tot
