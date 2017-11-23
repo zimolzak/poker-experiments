41cab62ba71894dffcddd6eb49515d650200731c
Andrew Zimolzak <andyzimolzak@gmail.com>
2017-11-23 11:45:16 -0500

    Distinguish pairs into subtypes (overpair, top pair, etc).

--- range_hits_board.py
+++ range_hits_board.py
@@ -3,32 +3,61 @@ from convenience import pr
 from deuces.deuces import Card, Evaluator
 
 e = Evaluator()
-
-basic_keys = []
+basic_keys = [e.class_to_string(i) for i in range(1,10)]
+fancy_keys = ['Straight Flush', 'Four of a Kind', 'Full House', 'Flush',
+              'Straight', 'Three of a Kind', 'Two Pair',
+              'Overpair', 'Top Pair', '1.5 Pair', 'Middle Pair', 'Weak Pair',
+              'High Card']
 rc_counts = {}
-for i in range(1,10):
-    s = e.class_to_string(i)
-    basic_keys.append(s)
-    rc_counts[s] = 0
 
-## Two input vars:
-board = [Card.new('Qs'), Card.new('Jd'), Card.new('2c')]
-range_list = ['AA', 'KK', 'QQ', 'AK', 'AKs']
+## Input vars:
+board = [Card.new('Qs'), Card.new('Td'), Card.new('4c')]
+range_list = ['AA', 'KK', 'QQ', 'AK', 'AKs', 'KQ', 'KQs', 'JJ', '33', '22', 'A4', '99']
+be_fancy = True
 
 ## tricky ones highlighted:
-##  1   2    3    4       5        6     7          8             9
-## sf quad boat flush straight trip set 2p overp tp 1.5p mp wp ah nmh
-##                             ^^^^^^^^    ^^^^^^^^^^^^^^^^^^^ ^^^^^^
+##  1   2    3    4       5        6     7          8                9
+## sf quad boat flush straight trip set 2p overp tp 1.5p mp wp overc ah nmh
+##                             ^^^^^^^^    ^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^
 
 print "Range:", range_list
 print "Board:",
 pr(board)
+
+def increment_dict(d, k):
+    if k in d.keys():
+        d[k] += 1
+    else:
+        d[k] = 1
+    return d
+
+def distinguish_pairs(hole, board):
+    # assumes no trips or set
+    hrs = [Card.get_rank_int(c) for c in hole]
+    brs = [Card.get_rank_int(c) for c in board]
+    brs.sort(reverse = True) # high card is index 0
+    if hrs[0] == max(brs) or hrs[1] == max(brs):
+        return 'Top Pair'
+    elif hrs[0] == hrs[1]: # pocket pair
+        if max(hrs) > max(brs):
+            return 'Overpair'
+        elif brs[0] > hrs[0] > brs[1]:
+            return '1.5 Pair'
+        else:
+            return 'Weak Pair'
+    elif brs[1] == hrs[0] or brs[1] == hrs[1]:
+        return 'Middle Pair'
+    else:
+        return 'Weak Pair'
+
 lol = all_hands_in_range(range_list)
 for L in lol:
     hr = e.evaluate(L, board)
     rc = e.get_rank_class(hr)
     s = e.class_to_string(rc)
-    rc_counts[s] += 1
+    if s == 'Pair' and be_fancy:
+        s = distinguish_pairs(L, board)
+    increment_dict(rc_counts, s)
 
 def pad_to(n, s):
     while len(s) < n:
@@ -37,6 +66,10 @@ def pad_to(n, s):
 
 print('\nResults\n========')
 denom = float(sum(rc_counts.values()))
-for s in basic_keys:
-    n = rc_counts[s]
-    print pad_to(15, s), n, '\t', round(n / denom * 100, 2)
+ks = basic_keys
+if be_fancy:
+    ks = fancy_keys
+for s in ks:
+    if s in rc_counts.keys():
+        n = rc_counts[s]
+        print pad_to(15, s), n, '\t', round(n / denom * 100, 2)
